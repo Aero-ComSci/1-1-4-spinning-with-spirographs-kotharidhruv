e31ff41addaa5857ae774943b40f421d007f70e5
Dhruv Kothari <86218408+kotharidhruv@users.noreply.github.com>
2024-09-11 15:00:35 -0700

    Update screensize.py

--- screensize.py
+++ screensize.py
@@ -1,21 +1,31 @@
 import turtle as trtl
+import random
 
 painter = trtl.Turtle()
 
 trtl.screensize(800,800)
 painter.speed(0)
-screen = [800,800]
 
-def drawSquares():
-    for i in range(4):
-        painter.forward(20)
-        painter.right(90)
+rainbow_colors = [ #AI Generated. Prompt: Generate a python list of 10 colors in the rainbow
+    "Red",
+    "Orange",
+    "Yellow",
+    "Green",
+    "Blue",
+]
 
-distance = screen[0]/5
+position = painter.pos()
 
-for i in range(5):
+for i in reversed(range(5)):
+    size = (i+1)*150
     painter.penup()
-    painter.goto(((i+1)*distance)-400,0)
+    painter.goto(position[0]-size/2,position[1]+size/2)
+    painter.begin_fill()
     painter.pendown()
-    drawSquares()
+    painter.fillcolor(random.choice(rainbow_colors))
+    for g in range(4):
+        painter.forward(size)
+        painter.right(90)
+    painter.end_fill()
 
+trtl.done()
